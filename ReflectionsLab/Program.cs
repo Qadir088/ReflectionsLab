f1881c5ea866b8e4bd5b723bee3fa5ee99caf2db
Qadir088 <qadircabbarli55@gmail.com>
2024-04-17 18:23:34 +0400

    task solved

---
+++ ReflectionsLab/Program.cs
@@ -0,0 +1,23 @@
+﻿using ReflectionsLab;
+using System;
+
+namespace ReflectionsLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student ali = new Student();
+            Console.WriteLine(ali.Age);
+
+            Student murad = new Student();
+            Console.WriteLine(murad.Age);
+
+            Student gadir = new Student();
+            Console.WriteLine(gadir.Age);
+
+            Student nicat = new Student();
+            Console.WriteLine(nicat.Age);
+        }
+    }
+}
---
+++ ReflectionsLab/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionsLab
+{
+    public class Student : Person
+    {
+        public Student()
+        {
+            lastAge++;
+            Age = lastAge;
+        }
+    }
+}
